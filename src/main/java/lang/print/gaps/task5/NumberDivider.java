9b988aec157d3046e2c481f4a7d5c5319c7cc581
nodircha <madnodiras23@gmail.com>
2025-02-07 10:28:22 +0500

    task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
+
     void divide(int toBeDivided) {
+        float num = toBeDivided / 5f;
+        System.out.println(num);
     }
 }
